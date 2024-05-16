36f46854e52d88e914106805b8e4609507875c22
SheerazMalik <techverseinsights786@gmail.com>
2024-05-16 14:36:31 +0500

    Variable by Sheraz Ahmad

---
+++ variable.py
@@ -0,0 +1,15 @@
+#Variable is a name of memory location which store of OR variable is storage of value OR variable is like a container which store value
+
+""" 1. We can not Represent with numbers like    986= 123
+    2. we can not used numbers at starts like..   23test = 10
+    3. we can not used Special keywords  like .. $#% =10
+"""
+
+name = "Sheraz Ahmad"
+age = 28
+      
+print(name)
+print(age)
+#another way of Represent variable#
+a,b =20,10
+print(a,b)
