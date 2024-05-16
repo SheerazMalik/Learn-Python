e4e1c288732fe3fb374b40c81f78ce8d710cfff1
SheerazMalik <techverseinsights786@gmail.com>
2024-05-16 15:26:52 +0500

    Sheraz Ahmad

---
+++ DataTypes.py
@@ -0,0 +1,12 @@
+#**Data Types**#
+#Numeric DataTypes 1. Int (Whole Number) 2. float(Deci numbers)#
+#used for performing basic operation#
+
+a=20 
+b=10
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
