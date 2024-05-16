33b22b1ddf1833387060746f9e62b8aa4de2044d
SheerazMalik <techverseinsights786@gmail.com>
2024-05-16 17:07:20 +0500

    Sheraz Ahmad

--- Augmented Assignments.py
+++ Augmented Assignments.py
@@ -1,63 +1,23 @@
-# Learn-Python
-Learn Python for EveryOne! </br></br>
 
-**What  is Indentation** </br></br>
-Concept of spacing. Normally 4 space like tab..</br>
-
-**Variable**</br>
-
-#Variable is a name of memory location which store Value OR variable is storage of value OR variable is like a container which store value
-
-""" 1. We can not Represent with numbers like    986= 123
-    2. we can not used numbers at starts like..   23test = 10
-    3. we can not used Special keywords  like .. $#% =10
-"""
-
-name = "Sheraz Ahmad"
-age = 28
-      
-print(name)
-print(age)
-#another way of Represent variable#
-a,b =20,10
-print(a,b)</br></br>
-
-**Data Types**</br>
-
-#Numeric DataTypes 1. Int (Whole Number) 2. float(Deci numbers)#
-#used for performing basic operation#
-
-a=20 
-b=10
-
-print(a+b)
-print(a-b)
-print(a/b)
-print(a//b)   will return whole number
-print(a%b)    Jo bhe baqia aya wo mean reminder</br></br>
-
-**Sequence of Operation**
-</br>
-
-**PEDMAS Rule**
-</br>
-1. Parenthesis 
-2. Exponent
-3. Division
-4. Multiplication
-5. Addition
-6. Subtration
-</br>
-
-**Augumented Assignment**
+""" **Augumented Assignment**
 
 An assignment operator is an operator that is used to assign some value to a variable. Like normally in Python, we write “a = 5“ to assign value 5 to variable ‘a’. Augmented assignment operators have a special role to play in Python programming. It basically combines the functioning of the arithmetic or bitwise operator with the assignment operator. So assume if we need to add 7 to a variable “a” and assign the result back to “a”, then instead of writing normally as “a = a + 7“, we can use the augmented assignment operator and write the expression as “a += 7“. Here += has combined the functionality of arithmetic addition and assignment.
 
 So, augmented assignment operators provide a short way to perform a binary operation and assigning results back to one of the operands. The way to write an augmented operator is just to write that binary operator and assignment operator together. In Python, we have several different augmented assignment operators like +=, -=, *=, /=, //=, **=, |=, &=, >>=, <<=, %= and ^=. Let’s see their functioning with the help of some exemplar codes:
-
-
-
-
-
-
-
+""" 
+a=10
+print(a)
+a+=2
+print(a)
+b=10
+b-=2
+print(b)
+c=10
+c/=2
+print(c)
+d=10
+d //=2
+print(d)
+e=10
+e**=2
+print(e)
